2b77758558946a76b779dc4d6795bd92c0b41fec
Samiylo <skryshu@gmail.com>
2021-05-31 17:37:12 -0500

    Mount properties.

--- src/Controller/FileWriterController.java
+++ src/Controller/FileWriterController.java
@@ -1,5 +1,27 @@
 package Controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriterController {
+	
+	// Grabbing file name and directory using io.File
+	
+	String directory = System.getProperty("user.home");
+	String fileName = "sample.txt";
+	String absolutePath = directory + File.separator + fileName;
+	
+	try(FileWriter fileWriter = new FileWriter(absolutePath)) {
+		
+		String fileContent = "This is sample text";
+		fileWriter.write(fileContent);
+		fileWriter.close();
+		
+	} catch (IOException e) {
+		System.out.println("Error in FileWriterController ::: " + e );
+	}
+	
+	
 
 }
