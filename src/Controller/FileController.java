23dc46ebc6fcb47c632c3efb111cbcff0f97ab5b
Samiylo <skryshu@gmail.com>
2021-06-01 12:37:26 -0500

    Read/Write complete.

--- src/Controller/FileController.java
+++ src/Controller/FileController.java
@@ -1,22 +1,45 @@
 package Controller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class FileController {
 	
 	
 	public File createFile(String fileName) {
-	
-		
-			System.out.println("In createFile() try/catch block");
+		System.out.println("In createFile() :::");
 			
-			File newFile = new File(fileName);
+		File newFile = new File(fileName); 
 			
-			
-			
-
 		return newFile;
 		
+	}
+	
+	public void writeToFile(File file, String info) throws IOException {
+		System.out.println("In writeToFile() :::");
+		
+		FileWriter fileWriter = new FileWriter(file);
+		fileWriter.write(info);
+		fileWriter.close();
+		
+		System.out.println("Written to file and fileWriter closed :::");
+	}
+	
+	public String readFile(File file) throws FileNotFoundException {
+		System.out.println("In readFile() :::");
+		String data = null;
+		
+			Scanner newScanner = new Scanner(file);
+			
+			while(newScanner.hasNextLine()) {
+				data = newScanner.nextLine();
+				
+			}
+	
+		return data;
 		
 	}
 
--- src/csvReadWrite/Main.java
+++ src/csvReadWrite/Main.java
@@ -1,21 +1,29 @@
 package csvReadWrite;
 
 import java.io.File;
+import java.io.IOException;
 
 import Controller.FileController;
 
 public class Main {
 	
 	@SuppressWarnings("unused")
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException {
 		
-		System.out.println("Hello World");
+		// System.out.println("Hello World");
 		
 		FileController fileController = new FileController();
 		
 		File newFile = fileController.createFile("myNewFile");
 		
-		System.out.println(newFile);
+
+		fileController.writeToFile(newFile, "Hello There :)");
+	
+		System.out.println(fileController.readFile(newFile));
+		
+		
+		
+		// System.out.println(newFile);
 	}
 
 }
