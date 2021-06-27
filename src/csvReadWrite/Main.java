a4127acc0af26cfc1794cf31a8709bbc08be1f50
Samiylo <skryshu@gmail.com>
2021-06-27 15:15:26 -0500

    Add Get

--- src/csvReadWrite/Main.java
+++ src/csvReadWrite/Main.java
@@ -1,8 +1,11 @@
 package csvReadWrite;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import Controller.FileController;
@@ -25,6 +28,31 @@ public class Main {
 	
 	public static void sendGET() throws IOException {
 		URL obj = new URL(getURL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", userAgent);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		
+		if (responseCode == HttpURLConnection.HTTP_ACCEPTED) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			
+			while((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			
+			//Print Result
+			System.out.println(response.toString());
+			
+		}
+		else {
+			System.out.println("GET request not working");
+		}
 	}
 
 }
