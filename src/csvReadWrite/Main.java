bca7e6157daa23c23b0c5130c909d69405a4f46c
Samiylo <skryshu@gmail.com>
2021-06-27 13:29:14 -0500

    Setup Request

--- src/csvReadWrite/Main.java
+++ src/csvReadWrite/Main.java
@@ -1,38 +1,30 @@
 package csvReadWrite;
 
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import Controller.FileController;
 
 public class Main {
 	
+	public static String userAgent = "Opera";
+	public static String getURL = "https://www.youtube.com/watch?v=bpAi70WWBlw";
+	public static String postURL = "";
+	public static String params = "";
+	
 	@SuppressWarnings("unused")
 	public static void main(String [] args) throws IOException {
 		
-		// System.out.println("Hello World");
+		sendGET();
+		System.out.println("GET DONE :::");
 		
 		
-		
-		FileController fileController = new FileController();
-		File newFile = fileController.createFile("myNewFile");
-		
-		
-		fileController.writeToFile(newFile, "Hello There :)");
-		
-		System.out.println("Here in readFile ::: " + fileController.readFile(newFile));
-		System.out.println("Here is the file path ::: " + newFile.getPath());
-		System.out.println("Here in isAbsolute() ::: " + newFile.isAbsolute());
-		System.out.println("Here in getAbsolutePath() ::: " + newFile.getAbsolutePath());
-		//System.out.println("Here in getCanonicalPath ::: " + newFile.getCanonicalPath());
-		
-		
-		// Delete on exit
-		System.out.println("Here in delteOnExit() ::: " + newFile.deleteOnExit(););
-		
-		
-		
-		// System.out.println(newFile);
+	}
+	
+	public static void sendGET() throws IOException {
+		URL obj = new URL(getURL);
 	}
 
 }
